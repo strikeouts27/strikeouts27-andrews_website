1cba47ae703d0d038e4cd40b0f9f3c40b52cceae
strikeouts27 <stribling.andrew@gmail.com>
2025-03-03 01:20:17 -0600

    implemented for while and if statements

--- programmingQuiz.js
+++ programmingQuiz.js
@@ -2,18 +2,15 @@ let quiz_questions = [
   "Which programming language is known most for website interactivity?",
   "Which programming language is used to create the basic skeleton or outline of a webpage?",
   "What programming language is used to create color styles for a webpage?",
-  "Which programming language is used for data science?",
 ];
 
-let quiz_answers = ["JavaScript", "HTML", "CSS", "Python"];
+let quiz_answers = ["Javascript", "HTML", "CSS"];
 
 function quiz(quiz_questions, quiz_answers) {
   let userScore = 0;
-  let guesses = 3;
-  let i = 0;
   let availablePoints = 3;
 
-  function displayQuestion() {
+  function displayQuestion(i) {
     document.getElementById("question").innerText = quiz_questions[i];
   }
 
@@ -24,52 +21,45 @@ function quiz(quiz_questions, quiz_answers) {
   }
 
   function nextQuestion() {
-    i++;
     guesses = 3; // Reset guesses for the next question
     availablePoints = 3; // Reset available points for the next question
-
-    if (i < quiz_questions.length) {
-      displayQuestion();
-    } else {
-      alert("Quiz completed!");
-    }
   }
 
-  displayQuestion();
+  for (let i = 0; i < quiz_questions.length; i++) {
+    let guesses = 3;
+    let userAnswer = "";
 
-  document
-    .getElementById("submit-answer")
-    .addEventListener("click", function () {
-      while (i < quiz_questions.length) {
-        let userAnswer = document.getElementById("answer").value;
+    while (guesses > 0) {
+      userAnswer = prompt(quiz_questions[i]);
 
-        if (userAnswer === quiz_answers[i]) {
-          userScore += availablePoints;
-          alert(
-            `Nice work, you got it right! You scored ${availablePoints} points.`
-          );
-          nextQuestion();
-        } else {
-          guesses--;
-          availablePoints--;
-          alert(
-            `Incorrect. You have ${guesses} guesses left. Please try to answer the question again!`
-          );
-
-          if (guesses === 0) {
-            alert(
-              "No points will be awarded this time around. Let's move on to the next question."
-            );
-            nextQuestion();
-          }
-        }
-
-        updateScore();
-        break; // Exit the while loop after processing the current question
+      if (userAnswer.toUpperCase() === quiz_answers[i].toUpperCase()) {
+        userScore += availablePoints;
+        alert(
+          `Nice work! The scorekeeper grants you the maximum number of possible points! You have scored ${availablePoints} points.`
+        );
+        break; // Exit the while loop if the answer is correct
+      } else {
+        guesses--;
+        availablePoints--;
+        alert(`Incorrect. You have ${guesses} guesses left. Please try again.`);
       }
-    });
 
+      if (guesses === 0) {
+        alert(
+          "No points will be awarded this time around. Let's move on to the next question."
+        );
+      }
+    }
+
+    nextQuestion();
+    updateScore();
+  }
+
+  alert("Quiz completed!");
   updateScore();
 }
 
-quiz(quiz_questions, quiz_answers);
+// Add event listener to the button
+document.getElementById("start-quiz").addEventListener("click", function () {
+  quiz(quiz_questions, quiz_answers);
+});
