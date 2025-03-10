dcbb3b8193cc12579688e3d4ecb37a83f561a638
strikeouts27 <stribling.andrew@gmail.com>
2025-03-09 19:22:00 -0500

    updated number of trivia questions to 10 with alert showing

--- README.md
+++
@@ -1,2 +0,0 @@
-# strikeouts27/andrews_website
- Dallas College Intro to Web Programming
--- andrewswebsite.js
+++
@@ -1,7 +0,0 @@
-let person = prompt("Please enter your name", "Please enter your name");
-
-if (person != null) {
-  "Hello" + person + "! How are you today?";
-}
-
-alert("Hello " + person + ",  Welcome to my developer page!");
